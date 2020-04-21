bf68c9b716d8fc024738075aa09354be541db8e3
Corina <corinacvp@gmail.com>
2020-04-21 19:57:37 -0400

    interface design to show options to filter

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,12 +15,28 @@ class Home extends Component {
             <div className='titleTwo'>
             <p>Busca aquí tu mascota ideal, la que haga mach con tu personalidad...</p>
             </div>
-            <div className='container_form'>
+            {/* <div className='container_form'>
             <form style={{display:'flex', alignItems:'center'}}>
                 <img className='buscar'src={buscar}/>
                 <input className='Search' type="search" placeholder="Buscar una raza..." />
             </form>
-            
+            </div> */}
+            <div className='raza'>
+            <p className='titleRaza'>Raza</p>
+            <hr/>    
+            <form style={{display:'flex', alignItems:'center', border: '1px solid gray', padding: 5, marginTop: 5 }}>
+                <img className='buscar'src={buscar}/>
+                <input className='Search' type="search" placeholder="Buscar una raza..." />
+            </form>
+            <label className='containerSelection'>
+                <div style={{display: 'flex', flexDirection:'row'}}>
+                <input type="checkbox"/> Chiguagua
+                </div>
+                <div style={{display: 'flex', flexDirection:'row'}}>
+                <input type="checkbox"/> Poodle
+                </div>
+            </label>
+
             </div>
     
 
