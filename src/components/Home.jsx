b75b7b05418c5ac083d68ac65c5e85a8719c67ae
Corina <corinacvp@gmail.com>
2020-04-22 09:48:09 -0400

    container subRaza

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,51 +1,65 @@
-import React, { Component } from 'react';
-import NavBar from './NavBar'
-import '../App.css'
-import '../assests/css/Home.css';
-import buscar from '../assests/img/buscar.svg'
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+import "../App.css";
+import "../assests/css/Home.css";
+import buscar from "../assests/img/buscar.svg";
 
 class Home extends Component {
-    render() {
-        return (
-            <>
-            <NavBar/>
-            <div className='container_home'>
-            <div style={{paddingLeft: '2em'}}>
-            <h1 className='title'>Encuentra tu mascota ideal</h1>
-            <div className='titleTwo'>
-            <p>Busca aquí tu mascota ideal, la que haga mach con tu personalidad...</p>
+  render() {
+    return (
+      <>
+        <NavBar />
+        <div className="container_home">
+          <div style={{ marginLeft: "2em" }}>
+            <h1 className="title">Encuentra tu mascota ideal</h1>
+            <div className="titleTwo">
+              <p>
+                Busca aquí tu mascota ideal, la que haga mach con tu
+                personalidad...
+              </p>
             </div>
-            {/* <div className='container_form'>
-            <form style={{display:'flex', alignItems:'center'}}>
-                <img className='buscar'src={buscar}/>
-                <input className='Search' type="search" placeholder="Buscar una raza..." />
-            </form>
-            </div> */}
-            <div className='raza'>
-            <p className='titleRaza'>Raza</p>
-            <hr/>    
-            <form style={{display:'flex', alignItems:'center', border: '1px solid gray', padding: 5, marginTop: 5 }}>
-                <img className='buscar'src={buscar}/>
-                <input className='Search' type="search" placeholder="Buscar una raza..." />
-            </form>
-            <label className='containerSelection'>
-                <div style={{display: 'flex', flexDirection:'row'}}>
-                <input type="checkbox"/> Chiguagua
+            <div style={{display: 'flex', flexDirection:'row'}}>
+            {/* Caja de Raza */}
+            <div className="raza">
+              <p className="titleRaza">Raza</p>
+              <hr />
+              <form style={{display: "flex", alignItems: "center",border: "1px solid gray",padding: 5,marginTop: 5 }}>
+                <img className="buscar" src={buscar} />
+                <input className="Search" type="search" placeholder="Buscar una raza..."/>
+              </form>
+              <label className="containerSelection">
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <input type="checkbox" /> Chiguagua
                 </div>
-                <div style={{display: 'flex', flexDirection:'row'}}>
-                <input type="checkbox"/> Poodle
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <input type="checkbox" /> Poodle
                 </div>
-            </label>
-
+              </label>
             </div>
-    
-
-
+            {/* Caja de Sub-Raza */}
+            <div className="sub-raza">
+              <p className="titleRaza">Sub Raza</p>
+              <hr />
+              <form style={{display: "flex", alignItems: "center",border: "1px solid gray",padding: 5,marginTop: 5 }}>
+                <img className="buscar" src={buscar} />
+                <input className="Search" type="search" placeholder="Buscar una raza..."/>
+              </form>
+              <label className="containerSelection">
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <input type="checkbox" /> Chiguagua
+                </div>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <input type="checkbox" /> Poodle
+                </div>
+              </label>
             </div>
             </div>
-            </>
-        );
-    }
+
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Home;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,25 @@
 import React, { Component } from "react";
 import "../assests/css/NavBar.css";
-import logo from '../assests/img/LOGOdog.png'
-import menu from '../assests/img/menuIcon.png'
+import logo from "../assests/img/LOGOdog.png";
+import menu from "../assests/img/menuIcon.png";
 import { findByLabelText } from "@testing-library/react";
 
+
 class NavBar extends Component {
+
   render() {
     return (
       <>
         <header>
-          <nav className='nav-links'>
-            <div className='containerLogo'>
-            <img style={{width:65, height:65}}src={logo}/>    
+          <nav className="nav-links">
+            <div className="containerLogo">
+              <img style={{ width: 65, height: 65 }} src={logo} />
             </div>
 
-            
-
-            <div className='containerLinks'>  
-            <a className='links'>Curiosidades</a>
-            <a className='links'>Salud</a>
-            <a className='links'>Alimentación</a>
-
+            <div className="containerLinks">
+              <a className="links">Curiosidades</a>
+              <a className="links">Salud</a>
+              <a className="links">Alimentación</a>
             </div>
           </nav>
         </header>
@@ -30,4 +29,3 @@ class NavBar extends Component {
 }
 
 export default NavBar;
-
