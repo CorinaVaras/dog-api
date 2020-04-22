8b87fdacfb776cb07c69e6920a4295cd22c29703
Corina <corinacvp@gmail.com>
2020-04-22 11:43:00 -0400

    printing dog breeds list on screen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,28 @@ import "../assests/css/Home.css";
 import buscar from "../assests/img/buscar.svg";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+    this.getData();
+  }
+
+  getData = () => {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        var result = Object.keys(data.message).map(function (key) {
+          return { name: key, sub: [data.message[key]] };
+        });
+      console.log(result);
+        this.setState({
+          data: result,
+        });
+      });
+  };
+
   render() {
     return (
       <>
@@ -18,43 +40,72 @@ class Home extends Component {
                 personalidad...
               </p>
             </div>
-            <div style={{display: 'flex', flexDirection:'row'}}>
-            {/* Caja de Raza */}
-            <div className="raza">
-              <p className="titleRaza">Raza</p>
-              <hr />
-              <form style={{display: "flex", alignItems: "center",border: "1px solid gray",padding: 5,marginTop: 5 }}>
-                <img className="buscar" src={buscar} />
-                <input className="Search" type="search" placeholder="Buscar una raza..."/>
-              </form>
-              <label className="containerSelection">
-                <div style={{ display: "flex", flexDirection: "row" }}>
-                  <input type="checkbox" /> Chiguagua
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              {/* Caja de Raza */}
+              <div className="raza">
+                <p className="titleRaza">Raza</p>
+                <hr />
+                <form
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    border: "1px solid gray",
+                    padding: 5,
+                    marginTop: 5,
+                  }}
+                >
+                  <img className="buscar" src={buscar} />
+                  <input
+                    className="Search"
+                    type="search"
+                    placeholder="Buscar una raza..."
+                  />
+                </form>
+                <div>
+                {this.state.data.map((dogs) => {
+                  return (
+                    
+                    <label key={dogs.name} className="containerSelection">
+                      <div style={{ display: "flex", flexDirection: "row"}}>
+                        <input type="checkbox" /> {dogs.name}
+                      </div>
+                    </label>
+                  );
+                })
+                }
                 </div>
-                <div style={{ display: "flex", flexDirection: "row" }}>
-                  <input type="checkbox" /> Poodle
-                </div>
-              </label>
-            </div>
-            {/* Caja de Sub-Raza */}
-            <div className="sub-raza">
-              <p className="titleRaza">Sub Raza</p>
-              <hr />
-              <form style={{display: "flex", alignItems: "center",border: "1px solid gray",padding: 5,marginTop: 5 }}>
-                <img className="buscar" src={buscar} />
-                <input className="Search" type="search" placeholder="Buscar una raza..."/>
-              </form>
-              <label className="containerSelection">
-                <div style={{ display: "flex", flexDirection: "row" }}>
-                  <input type="checkbox" /> Chiguagua
-                </div>
-                <div style={{ display: "flex", flexDirection: "row" }}>
-                  <input type="checkbox" /> Poodle
-                </div>
-              </label>
-            </div>
-            </div>
+              </div>
 
+              {/* Caja de Sub-Raza */}
+              <div className="sub-raza">
+                <p className="titleRaza">Sub Raza</p>
+                <hr />
+                <form
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    border: "1px solid gray",
+                    padding: 5,
+                    marginTop: 5,
+                  }}
+                >
+                  <img className="buscar" src={buscar} />
+                  <input
+                    className="Search"
+                    type="search"
+                    placeholder="Buscar una raza..."
+                  />
+                </form>
+                <label className="containerSelection">
+                  <div style={{ display: "flex", flexDirection: "row" }}>
+                    <input type="checkbox" /> Chiguagua
+                  </div>
+                  <div style={{ display: "flex", flexDirection: "row" }}>
+                    <input type="checkbox" /> Poodle
+                  </div>
+                </label>
+              </div>
+            </div>
           </div>
         </div>
       </>
