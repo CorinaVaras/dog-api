9c3866c016a3ccc6b7f986d602eb45d3c2711fb9
Corina <corinacvp@gmail.com>
2020-04-28 10:14:39 -0400

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,17 @@ import "../App.css";
 import "../assets/css/Home.css";
 import buscar from "../assets/img/buscar.svg";
 import random from "../assets/img/random.jpg";
+
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      data: [], 
-      tags: [], 
-      dataFilter: [], 
-      allData: [] 
+    this.state = {
+      data: [],
+      tags: [],
+      dataFilter: [],
+      allData: [],
     };
+
     this.getData();
   }
 
@@ -23,13 +25,13 @@ class Home extends Component {
       })
       .then((data) => {
         let result = Object.keys(data.message).map(function (raza) {
-          let subRazas = Object.values(data.message[raza]).map(function (subRaza) {
+          let subRazas = Object.values(data.message[raza]).map(function (
+            subRaza
+          ) {
             return { name: subRaza };
           });
           return { name: raza, sub: subRazas };
         });
-
-        // console.log(result);
 
         let allDogs = [];
 
@@ -45,9 +47,7 @@ class Home extends Component {
                 dataFilter: [...previousState.dataFilter, dog],
                 allData: [...previousState.allData, dog],
               }));
-              
-            })
-            
+            });
 
           item.sub.map((sub) => {
             fetch(`https://dog.ceo/api/breed/${sub.name}/images`)
@@ -70,11 +70,11 @@ class Home extends Component {
           data: result,
         });
 
-        console.log('>>>' + JSON.stringify(this.state.allData))
+        console.log(">>>" + JSON.stringify(this.state.allData));
       });
   };
 
-  // evento al input checkbox de las
+  // evento al input checkbox
   handleChange = (event) => {
     const value = event.target.name;
 
@@ -94,17 +94,12 @@ class Home extends Component {
     });
 
     this.filterData();
-
-    // setTimeout(()=>{
-    //     console.log(this.state.tags);
-    // },500)
   };
 
   filterData = () => {
     let currentAllData = this.state.allData;
 
     if (this.state.tags.length == 0) {
-
       this.setState({
         dataFilter: this.state.allData,
       });
@@ -123,120 +118,116 @@ class Home extends Component {
     return (
       <>
         <NavBar />
-        <div className='container-texto'>
-        <h1 className='title'>Encuentra tu mascota ideal aquí</h1>
-        <p className='titletwo'>La que haga mach con tu personalidad!!!</p>
+        <div className="container-texto">
+          <h1 className="title">Encuentra tu mascota ideal aquí</h1>
+          <p className="titletwo">La que haga mach con tu personalidad!!!</p>
         </div>
         <div className="container_home">
-            {/* CAJA DE LAS RAZAS */}
-            <div className='test'>
-              <div className="raza">
-                <p className="titleRaza">Razas</p>
-                <hr />
-                <form style={{display: "flex", alignItems: "center",border: "1px solid gray", padding: 5, marginTop: 5 }}>
-                  <img className="buscar" src={buscar} />
-                  <input className="Search" type="search" placeholder="Buscar una raza..."/>
-                </form>
+          {/* CAJA DE LAS RAZAS */}
+          <div className="test">
+            <div className="raza">
+              <p className="titleRaza">Razas</p>
+              <hr />
+              <form
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  border: "1px solid gray",
+                  padding: 5,
+                  marginTop: 5,
+                }}
+              >
+                <img className="buscar" src={buscar} />
+                <input
+                  className="Search"
+                  type="search"
+                  placeholder="Buscar una raza..."
+                />
+              </form>
 
-                <div className="container-dogs">
-                  {this.state.data.map((dogs) => {
-                    return (
-                      <label key={dogs.name} className="containerSelection">
-                        <div style={{ display: "flex", flexDirection: "row" }}>
-                          <input
-                            onChange={(event) => this.handleChange(event)}
-                            type="checkbox"
-                            name={dogs.name}
-                          />{" "}
-                          {dogs.name}
-                        </div>
-                      </label>
-                    );
-                  })}
-                </div>
-              </div>
-            </div>
-            {/* CAJA DE LA SUB RAZA */}
-            <div className='test'>
-              <div className="raza">
-                <p className="titleRaza">Sub-Razas</p>
-                <hr />
-                <form style={{display: "flex", alignItems: "center",border: "1px solid gray", padding: 5, marginTop: 5 }}>
-                  <img className="buscar" src={buscar} />
-                  <input className="Search" type="search" placeholder="Buscar una raza..."/>
-                </form>
-
-                <div className="container-dogs">
-                  {this.state.data.map((dogs) => (
-                    dogs.sub.map(sub => (
-                        <label key={sub.name} className="containerSelection">
-                          <div style={{ display: "flex", flexDirection: "row" }}>
-                            <input
-                              onChange={(event) => this.handleChange(event)}
-                              type="checkbox"
-                              name={sub.name}
-                            />{" "}
-                            {sub.name}
-                          </div>
-                        </label>
-                    ))
-                  ))}
-                </div>
-              </div>
-            </div>
-            {/* CAJA DONDE SE MUESTRAN LOS PERROS CON IMAGENES */}
-            <div className='test'>
-            <div className="DogsContainer">
-                {this.state.dataFilter.map((item) => {
+              <div className="container-dogs">
+                {this.state.data.map((dogs) => {
                   return (
-                    <div className="card-dogs">
-                      {item.img == "B" ? (
-                        <img
-                          style={{ width: "110px", height: "110px" }}
-                          src={random}
-                        />
-                      ) : (
-                        <img
-                          style={{ width: "110px", height: "110px" }}
-                          src={item.img}
-                        />
-                      )}
-                      <p>{item.name}</p>
-                    </div>
+                    <label key={dogs.name} className="containerSelection">
+                      <div style={{ display: "flex", flexDirection: "row" }}>
+                        <input
+                          onChange={(event) => this.handleChange(event)}
+                          type="checkbox"
+                          name={dogs.name}
+                        />{" "}
+                        {dogs.name}
+                      </div>
+                    </label>
                   );
                 })}
               </div>
-              </div>
-
+            </div>
           </div>
-          </>
+          {/* CAJA DE LA SUB RAZA */}
+          <div className="test">
+            <div className="raza">
+              <p className="titleRaza">Sub-Razas</p>
+              <hr />
+              <form
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  border: "1px solid gray",
+                  padding: 5,
+                  marginTop: 5,
+                }}>
+                <img className="buscar" src={buscar} />
+                <input
+                  className="Search"
+                  type="search"
+                  placeholder="Buscar una raza..."
+                />
+              </form>
+              <div className="container-dogs">
+                {this.state.data.map((dogs) =>
+                  dogs.sub.map((sub) => (
+                    <label key={sub.name} className="containerSelection">
+                      <div style={{ display: "flex", flexDirection: "row" }}>
+                        <input
+                          onChange={(event) => this.handleChange(event)}
+                          type="checkbox"
+                          name={sub.name}
+                        />{" "}
+                        {sub.name}
+                      </div>
+                    </label>
+                  ))
+                )}
+              </div>
+            </div>
+          </div>
+          {/* CAJA DONDE SE MUESTRAN LOS PERROS CON IMAGENES */}
+          <div className="test">
+            <div className="DogsContainer">
+              {this.state.dataFilter.map((item) => {
+                return (
+                  <div className="card-dogs">
+                    {item.img == "B" ? (
+                      <img
+                        style={{ width: "110px", height: "110px" }}
+                        src={random}
+                      />
+                    ) : (
+                      <img
+                        style={{ width: "110px", height: "110px" }}
+                        src={item.img}
+                      />
+                    )}
+                    <p>{item.name}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </>
     );
-
-             
   }
 }
 
 export default Home;
-
-              {/* <div className="DogsContainer">
-                {this.state.dataFilter.map((item) => {
-                  return (
-                    <div className="card-dogs">
-                      {item.img == "B" ? (
-                        <img
-                          style={{ width: "100px", height: "95px" }}
-                          src={random}
-                        />
-                      ) : (
-                        <img
-                          style={{ width: "100px", height: "95px" }}
-                          src={item.img}
-                        />
-                      )}
-                      <p>{item.name}</p>
-                    </div>
-                  );
-                })}
-              </div> */}
-            
-    
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,46 +2,55 @@ import React, { Component } from "react";
 import "../assets/css/NavBar.css";
 import logo from "../assets/img/logo.svg";
 import menu from "../assets/img/menuIcon.png";
-import { findByLabelText } from "@testing-library/react";
+
 
 class NavBar extends Component {
-  
   render() {
-    return <>
-     <header>
-       
-        <div className='logoMenu-container'>
-        <label htmlFor="toggle"><img style={{width: '40px', height:'40px'}} alt="menuIcon" src={menu}/></label>
-        </div>
-        <input type="checkbox" id="toggle" />
-        
+    return (
+      <>
+        <header>
+          <div className="logoMenu-container">
+            <label htmlFor="toggle">
+              <img
+                style={{ width: "40px", height: "40px" }}
+                alt="menuIcon"
+                src={menu}
+              />
+            </label>
+          </div>
+          <input type="checkbox" id="toggle" />
+
           <nav className="nav-links">
-            <div className='container-logo'>
-            <img className="logo" alt="logoMoov" src={logo} href="#"/>
+            <div className="container-logo">
+              <img className="logo" alt="logoMoov" src={logo} href="#" />
             </div>
-    
 
             <div>
-            <ul>
-              <div className='containerElemt'> 
-              <div><li>Alimentación</li></div>
-              </div>
+              <ul>
+                <div className="containerElemt">
+                  <div>
+                    <li>Alimentación</li>
+                  </div>
+                </div>
 
-              <div className='containerElemt'>
-              <div href="#Favorities"><li>Salud</li></div>
-              </div>
+                <div className="containerElemt">
+                  <div href="#Favorities">
+                    <li>Salud</li>
+                  </div>
+                </div>
 
-              <div className='containerElemt'>
-              <div href="#About"><li>Curiosidades</li></div>
-              </div>
-            </ul>
+                <div className="containerElemt">
+                  <div href="#About">
+                    <li>Curiosidades</li>
+                  </div>
+                </div>
+              </ul>
             </div>
           </nav>
-      </header>
-    </>;
+        </header>
+      </>
+    );
   }
 }
 
 export default NavBar;
-
-
